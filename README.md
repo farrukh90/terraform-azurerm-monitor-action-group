4186be090a259b5757ddca35dca61fe98b956fc8
farrukh90 <farrukhsadykov@gmail.com>
2021-07-11 19:55:46 -0500

    updated readme

--- README.md
+++ README.md
@@ -12,18 +12,16 @@ resource "azurerm_resource_group" "example" {
 }
 
 module "actiongroup" {
-    depends_on = [azurerm_resource_group.example]
-    source = "farrukh90/monitor-action-group/azurerm""
-    resource_group_name     = azurerm_resource_group.example.name
-    actiongroup_name        = "actiongroup"
-    email_address           = "email@gmail.com"
-
-    tags = {
+  depends_on          = [azurerm_resource_group.example]
+  source              = "../"
+  resource_group_name = azurerm_resource_group.example.name
+  actiongroup_name    = "actiongroup"
+  email_address       = "email@gmail.com"
+  tags = {
     Name        = "Alert"
     Environment = "Dev"
     Team        = "DevOps"
     Department  = "IT"
-    }
+  }
 }
-
 ```
--- examples/module.tf
+++ examples/module.tf
@@ -2,30 +2,24 @@ provider "azurerm" {
   features {}
 }
 
-module "rg" {
-  source   = "farrukh90/resource-group/azurerm"
-  name     = "dev"
-  location = "westus2"
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+module "actiongroup" {
+  depends_on          = [azurerm_resource_group.example]
+  source              = "../"
+  resource_group_name = azurerm_resource_group.example.name
+  actiongroup_name    = "actiongroup"
+  email_address       = "email@gmail.com"
+
   tags = {
     Name        = "Alert"
     Environment = "Dev"
     Team        = "DevOps"
     Department  = "IT"
   }
-}
 
-module "actiongroup" {
-    depends_on = [module.rg]
-    source = "../"
-    resource_group_name     = module.rg.resource_group_name
-    actiongroup_name        = "actiongroup"
-    email_address           = "email@gmail.com"
-
-    tags = {
-    Name        = "Alert"
-    Environment = "Dev"
-    Team        = "DevOps"
-    Department  = "IT"
-    }
-    
 }
